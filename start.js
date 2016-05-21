a57574f27e4f00567b31992c8ea03c922bd2fd21
agumurzhi <a.gumurzhi@gmail.com>
2016-05-21 11:37:43 +0300

    2.0

--- start.js
+++ start.js
@@ -7,25 +7,43 @@ var dbTestRec = new dbService(testRec);
 function bz() {
     var rec = {
         name: 'first',
-        rec: 33
+        rec: Math.random()
     };
-    dbTestRec.findOne({name: 'first'})
-        .then(function (data) {
-            if (data) {
-                return dbTestRec.findAndUpdate({_id: data._id}, {rec: Math.random()})
-            } else {
-                return dbTestRec.createNew(rec);
-            }
-        })
+    // dbTestRec.findOne({name: 'first'})
+    //     .then(function (data) {
+    //         if (data) {
+    //             return dbTestRec.findAndUpdate({_id: data._id}, {rec: Math.random()})
+    //         } else {
+    //             return dbTestRec.createNew(rec);
+    //         }
+    //     })
+    //     .then(function (answer) {
+    //         logger.info(answer);
+    //         setTimeout(function () {
+    //             bz();
+    //         }, 100);
+    //     })
+    //     .catch(function (err) {
+    //         logger.error(err);
+    //     });
+    dbTestRec.find({})
+    .then(function(data){
+       if (data.length && data.length > 30){
+           logger.trace(Math.floor(Math.random() * data.length));
+           var daletedObj = data[Math.floor(Math.random() * data.length)];
+           return dbTestRec.remove({_id: daletedObj._id});
+       } else {
+           return dbTestRec.createNew(rec);
+       }
+
+    })
         .then(function (answer) {
             logger.info(answer);
-            setTimeout(function () {
-                bz();
-            }, 100);
+            bz();
         })
-        .catch(function (err) {
-            logger.error(err);
-        });
+    .catch(function(err){
+        logger.error(err);
+    });
 }
 bz();
 
